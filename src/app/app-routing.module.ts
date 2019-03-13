462e08e08d453a133638bcea7d5ae3bc6a4bdcbc
Rami Al-Zebak <17zrami@gmail.com>
2019-03-13 20:48:02 +0200

    use hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)  , AuthModule ],
+  imports: [RouterModule.forRoot(routes , {useHash : true })  , AuthModule ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
